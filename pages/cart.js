68cdfa263190bf38aba747f4cebe565eec6052c7
quyld17 <quyld17@gmail.com>
2023-05-22 15:36:05 +0700

    first commit

---
+++ pages/cart.js
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function CartPage() {
+    return (
+        <p>Back to <Link href="/">homepage</Link></p>
+    );
+}
---
+++ pages/profile.js
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function ProfilePage() {
+    return (
+        <p>Back to <Link href="/">homepage</Link></p>
+    );
+}
